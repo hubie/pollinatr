887c2b56328edeb5095e74b7589b67211ef9d7ae
David Huebner <dhuebner@bushelpowered.com>
2022-11-27 20:08:43 -0600

    Fix logic issue with setting tenant_id

--- lib/pollinatr_web/plugs/tenant.ex
+++ lib/pollinatr_web/plugs/tenant.ex
@@ -8,21 +8,28 @@ defmodule PollinatrWeb.Plug.Tenant do
   end
 
 
-  def call(conn, %{use_default: use_default}) do
-    IO.inspect("should_call")
-    case get_session(conn, :tenant_id) do
+  def call( conn, [use_default: true]) do
+    case get_session(conn, "tenant_id") do
       nil ->
-        if( use_default == true) do
-          tenant_id = get_default_tenant_id()
-          IO.inspect(tenant_id, label: "setting default_tenant_id")
-          Repo.put_tenant_id(tenant_id)
-          conn
-          |> put_session(:tenant_id, tenant_id)
-        end
+        tenant_id = get_default_tenant_id()
+        IO.inspect(tenant_id, label: "setting default_tenant_id")
+        Repo.put_tenant_id(tenant_id)
+        conn |> put_session(:tenant_id, tenant_id)
+      tenant_id ->
+        Repo.put_tenant_id(tenant_id)
+        conn
+    end
+  end
+
+  def call( conn, _args ) do
+    case get_session(conn, "tenant_id") do
+      nil ->
+        # TODO: create state for no valid tenant
+        IO.inspect("No default handler for _no_ tenant yet")
       tenant_id ->
         # TODO: Should validate the tenant_id
         Repo.put_tenant_id(tenant_id)
-        conn
+    conn
     end
   end
 
